6ff439c410c7e8c0517f50fd6f47106ca68b24bf
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2019-12-18 16:23:45 +0100

    Update exppareto.R

--- exppareto.R
+++ exppareto.R
@@ -26,7 +26,7 @@ cppFunction(depends="RcppArmadillo",plugins="cpp11",code="
               }
               return(e);
             }
-            ") #multiniveaux avec critère d'arrêt
+            ") 
 
 
 
